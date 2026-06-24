09e5b2f2a34a530df942096159eea4570553bc56
r.mikawa <mikawa@hope-road.co.jp>
2026-06-24 22:37:10 +0900

    initial commit

---
+++ src/scripts/vendors.js
@@ -0,0 +1,10 @@
+import Splide from '@splidejs/splide';
+import GLightbox from 'glightbox';
+import imagesLoaded from 'imagesloaded';
+import Alpine from 'alpinejs';
+
+window.Splide = Splide;
+window.GLightbox = GLightbox;
+window.imagesLoaded = imagesLoaded;
+window.Alpine = Alpine;
+Alpine.start();
